841c14b71ce64d080606ca6ec2290e36d7add8b3
yukawahara0416 <yu.kawahara0416@gmail.com>
2021-09-14 11:05:06 +0900

    improve: prettierignore解除

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,10 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
+    node: true
   },
   extends: ['eslint:recommended', 'prettier'],
   rules: {
-    'no-undef': 'off',
-  },
+    'no-undef': 'off'
+  }
 };
